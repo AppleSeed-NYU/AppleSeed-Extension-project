4e509433e3759207bb0e675e60b3f8133d398de3
nijialu <izoeyz@163.com>
2021-12-14 23:48:58 -0500

    modify

--- popup.js
+++ popup.js
@@ -27,7 +27,7 @@ var myButton = document.getElementById("water");
 console.log("got water button", myButton)
 if(myButton){
     console.log(" button clicked")
-    myButton.addEventListener("click", writeToDatabase, false);}
+    myButton.addEventListener("click", waterFunction, false);}
 
 // document.getElementById("water").onclick = function waterFunction(userId, name, points, trees) {
 //     console.log("writing user data")
@@ -51,26 +51,26 @@ if(myButton){
  
 
 // test function
-function writeToDatabase(){
-    console.log("writing user data")
-    firebase.database().ref('/' ).set({
-      id: "2",
-      name: "new user",
-      points: 19,
-      trees : "id2"
-    });
-}
+// function writeToDatabase(){
+//     console.log("writing user data")
+//     firebase.database().ref('/' ).set({
+//       id: "2",
+//       name: "new user",
+//       points: 19,
+//       trees : "id2"
+//     });
+// }
 
 
 function waterFunction(){
     // minus 1 points
-    console.log("writing user data")
-    firebase.database().ref('users/' ).set({
-      id: "2",
-      name: "new user",
-      points: 19,
-      trees : "id2"
-    });
+    // console.log("writing user data")
+    // firebase.database().ref('users/' ).set({
+    //   id: "2",
+    //   name: "new user",
+    //   points: 19,
+    //   trees : "id2"
+    // });
 
     chrome.action.setPopup({
         popup: "tree_growing1.html"
